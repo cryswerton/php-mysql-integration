a2955fd5a539d8326160b980756b9460b22cf223
cryswerton <cryswertons@gmail.com>
2021-09-04 21:42:41 -0300

    added simple responsiveness

--- action.php
+++ action.php
@@ -1,26 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
 
-// DON'T forget to create you're database-info.php file.
-include 'database-info.php';
+    <?php
 
-// Create connection
-$conn = mysqli_connect($host, $user, $password, $database);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+        // DON'T forget to create you're database-info.php file.
+        include 'database-info.php';
 
-$word = $_POST['word'];
-$meaning = $_POST['meaning'];
-$example = $_POST['example'];
+        // Create connection
+        $conn = mysqli_connect($host, $user, $password, $database);
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
 
-$sql = "INSERT INTO words (name, meaning, example)
-VALUES ('$word', '$meaning', '$example')";
+        $word = $_POST['word'];
+        $meaning = $_POST['meaning'];
+        $example = $_POST['example'];
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
+        $sql = "INSERT INTO words (name, meaning, example)
+        VALUES ('$word', '$meaning', '$example')";
 
-$conn->close();
+        if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+        } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+    ?>
+
+</body>
+</html>
